8ae9a7b5efeeee28dfc7fc6cb50c9e1df3198bfe
Anna Eklund <vitastjern@gmail.com>
2021-05-27 18:41:06 +0200

    Just fixing minor things

--- user_proj/user_proj/urls.py
+++ user_proj/user_proj/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from user_app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^users/', views.users, name='users'),
+    url(r'^admin/', admin.site.urls),
 ]
